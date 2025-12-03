c3ccccf4160355e664522c970c0c7f2d5fda5aa0
Predrag Jovanovic <pjovanovic@siber.com>
2025-12-03 14:12:54 +0100

    Fix useNavigate()

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
-// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/app/App"; 
+import App from "./app/App";
 
-const rootElement = document.getElementById("root");
+import { BrowserRouter } from "react-router-dom";
 
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-
-  root.render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  );
-}
+    </BrowserRouter>
+  </React.StrictMode>
+);
