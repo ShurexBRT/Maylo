a352c12b2ece61bdcaa664834add71e1a9ebf56e
Predrag Jovanovic <pjovanovic@siber.com>
2025-12-03 09:34:14 +0100

    izbacivanje virtual:pwa-register iz main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,9 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { registerSW } from "virtual:pwa-register";
 import App from "./app/App";
 import "./styles/global.css";
 
-// service worker registration (PWA auto-update)
-registerSW({
-  immediate: true,
-  onNeedRefresh() {
-    // TODO: optional toast: "New version available"
-  },
-  onOfflineReady() {
-    // TODO: optional toast: "App is ready offline"
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
