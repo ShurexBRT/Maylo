b17812c95bac67dcc1114e017289347fc80ac203
ShurexBRT <jovanovicpedja676@gmail.com>
2025-12-16 20:02:07 +0100

    fix: Drawer items removed

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -45,31 +45,7 @@ export default function Drawer() {
       <nav className="p-4 text-sm">
         <ul className="space-y-1">
           {/* MAIN NAV */}
-          <li>
-            <Link
-              to="/"
-              onClick={handleClose}
-              className={`block rounded-md px-4 py-2 ${
-                isActive("/") ? "bg-blue-50 text-blue-700" : "hover:bg-gray-50"
-              }`}
-            >
-              Home
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              to="/search"
-              onClick={handleClose}
-              className={`block rounded-md px-4 py-2 ${
-                isActive("/search")
-                  ? "bg-blue-50 text-blue-700"
-                  : "hover:bg-gray-50"
-              }`}
-            >
-              Search
-            </Link>
-          </li>
+      
 
           {userId && (
             <li>
