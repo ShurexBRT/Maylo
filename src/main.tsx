6fee2818ce7164742c059c4f6694acd2d0730b83
ShurexBRT <jovanovicpedja676@gmail.com>
2025-12-16 21:40:12 +0100

    fix: Main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
-
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./app/App";
+import "./styles/globals.css";
+
+// jedan query client za celu app
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
